3564aa79e436221aadfca6c077a25ba8deeb24f2
StealthC <cacefmail@gmail.com>
2025-05-15 13:21:46 +0000

    Add padding to footer for improved spacing

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-sm text-center">
+  <div class="text-sm text-center pb-6">
     Feito com ❤️ por Carlos Alberto Castelo @ IFCE <em>Campus</em> Canindé (<a
       class="hover:underline"
       href="https://github.com/IFCE-Campus-Caninde/calculo-progressoes"
