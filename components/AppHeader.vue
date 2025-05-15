e83c9298b128d95360873af2c095e6cdf26bcf6e
StealthC <cacefmail@gmail.com>
2025-05-15 14:12:08 +0000

    Fix header title to reflect correct terminology for merit-based progression

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,7 +2,7 @@
   <div class="p-2 flex flex-row justify-between">
     <div class="flex-1">
       <h1 class="text-3xl font-bold text-center mt-2 mb-0">
-        Calculadora de Progressão por Capacitação para TAE
+        Calculadora de Progressão por Mérito para TAE
       </h1>
       <h2 class="text-lg sm:text-xl text-center font-medium">
         (Válida para progressões a partir de 2025)
